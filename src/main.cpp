9bb49d533efcc273b5542f155013258c0246011f
JOHN LAWRENCE ABENOJA <jlabenoja28@gmail.com>
2025-12-23 20:46:34 +0800

    Changes in Entity

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,45 @@
+
+#ifndef ENTITY_H
+#define ENTITY_H
+
 #include <iostream> 
+#include <string>
+using namespace std;
+
+class Entity {
+public:
+    string name;
+    int health;
+    int maxHealth;
+    int damage;
+    int level;
+
+    Entity(string n, int h, int d){
+        name = n;
+        maxHealth = h;
+        health = h;
+        damage = d;
+        level = 1;
+    }
+
+    // check if dead
+    bool isAlive(){
+        return health > 0;
+    }
+
+    // Taking Damage logic
+    void takeDamage(int dmg){
+        health -= dmg;
+        if (health < 0)
+            health = 0;
+    }
+
+    //healing logic
+    void heal(int amount){
+        health =+ amount;
+        if (health > maxHealth)
+            health = maxHealth;
+    }
+};
+
+#endif
