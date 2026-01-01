fbf1d48bf28ac6e508999116ee65a1bbfe7f8e6b
JOHN LAWRENCE ABENOJA <jlabenoja28@gmail.com>
2026-01-01 16:11:13 +0800

    changes

--- src/main.cpp
+++ src/main.cpp
@@ -10,5 +10,23 @@ int main(){
 
     cout<<"A Wild "<< goblin.name <<" appears!" <<endl;
     cout<<"It has "<<goblin.health << " HP." <<endl;
+
+        cout<<"Is the goblin alive? "<<endl;
+    if(goblin.isAlive() == true){
+        cout<<"Yes it is alive!"<<endl;
+    }
+    else
+        cout<<"Oh, it's dead!"<<endl;
+
+    goblin.takeDamage(50);
+
+    cout<<"It has "<<goblin.health << " HP." <<endl;
+
+    cout<<"Is the goblin alive? "<<endl;
+    if(goblin.isAlive() == true){
+        cout<<"Yes it is alive!"<<endl;
+    }
+    else
+        cout<<"Oh, it's dead!"<<endl;
 }
 
